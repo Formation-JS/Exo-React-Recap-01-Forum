ea112ec945fb9b1dc734ba55c9987b9cc62f2ef0
Aurélien <aurelien.strimelle@bstorm.be>
2022-04-26 11:25:03 +0200

    about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,7 +1,9 @@
 const AboutPage = () => {
-    return (
-        <h1>Je suis sur la page à propos</h1>
-    );
+    return (<>
+        <h1>A propos</h1>
+        <p>Ceci est un exercice en React durant la formation Javascript Full Stack à BXL Formation &#10084;</p>
+        <p>Cette application utilise une API en Node/Express</p>
+    </>);
 };
 
 export default AboutPage;
